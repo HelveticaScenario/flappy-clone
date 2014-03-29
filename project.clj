0a9f4dff9c2e6aa57630356c7de8f523745b3355
micimize <michael.the.awesome@gmail.com>
2014-03-28 19:49:24 -0500

    added monet

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]]
+                 [org.clojure/clojurescript "0.0-2138"]
+                 [rm-hull/monet "0.1.10"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "flappy-clone"
               :source-paths ["src"]
               :compiler {
--- src/flappy_clone/core.cljs
+++ src/flappy_clone/core.cljs
@@ -1,5 +1,7 @@
-(ns flappy-clone.core)
+(ns flappy-clone.core
+  :require [monet.canvas :as canvas])
 
 (enable-console-print!)
-
 (println "Hello world!")
+
+(def canvas-dom (.getElement js/document "#canvas"))
