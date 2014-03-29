6f5a709fdeccf3af3158c4b891021b60f74080b3
Daniel Lewis <thecontrarian42@gmail.com>
2014-03-28 21:14:46 -0500

    makeing moving stuff

--- src/flappy_clone/core.cljs
+++ src/flappy_clone/core.cljs
@@ -6,9 +6,11 @@
 (def surface (canvas/init (.getElementById js/document "surface") "2d"))
 
 (canvas/add-entity surface :background
-                   (canvas/entity {:x 0 :y 0 :w 600 :h 600} ; val
-                                  nil                       ; update function
+                   (canvas/entity {:x 0 :y 0 :w 60 :h 60} ; val
+                                  (fn [{:keys [x y] :as val}]
+                                      (assoc val :x (+ x 5) :y (+ y 5)))                       ; update function
                                   (fn [ctx val]             ; draw function
                                     (-> ctx
                                         (canvas/fill-style "#191d21")
                                         (canvas/fill-rect val)))))
+
