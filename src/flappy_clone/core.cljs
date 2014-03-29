d4ecd96006420181ad504694b3354c8e5f6a1f6b
Daniel Lewis <thecontrarian42@gmail.com>
2014-03-28 19:53:43 -0500

    stuff

--- src/flappy_clone/core.cljs
+++ src/flappy_clone/core.cljs
@@ -1,14 +1,17 @@
 (ns flappy-clone.core
-  (:require [goog.events :as events]
-            [goog.dom :as dom]))
+  (:require [monet.canvas :as canvas]))
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def canvas-dom (.getElementById js/document "surface"))
 
-(defn surface []
-  (let [surface (dom/getElement "surface")]
-    [(.getContext surface "2d")
-     (. surface -width)
-     (. surface -height)]))
+(def monet-canvas (canvas/init canvas-dom "2d"))
+
+(canvas/add-entity monet-canvas :background
+                   (canvas/entity {:x 0 :y 0 :w 600 :h 600} ; val
+                                  nil                       ; update function
+                                  (fn [ctx val]             ; draw function
+                                    (-> ctx
+                                        (canvas/fill-style "#191d21")
+                                        (canvas/fill-rect val)))))
 
