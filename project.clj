319b1643e6c6a5dedaad664c54a244b0ec2f0f9d
Daniel Lewis <thecontrarian42@gmail.com>
2014-03-28 19:47:01 -0500

    added stuff

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]]
+                 [org.clojure/clojurescript "0.0-2173"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "flappy-clone"
               :source-paths ["src"]
               :compiler {
--- src/flappy_clone/core.cljs
+++ src/flappy_clone/core.cljs
@@ -1,5 +1,14 @@
-(ns flappy-clone.core)
+(ns flappy-clone.core
+  (:require [goog.events :as events]
+            [goog.dom :as dom]))
 
 (enable-console-print!)
 
 (println "Hello world!")
+
+(defn surface []
+  (let [surface (dom/getElement "surface")]
+    [(.getContext surface "2d")
+     (. surface -width)
+     (. surface -height)]))
+
